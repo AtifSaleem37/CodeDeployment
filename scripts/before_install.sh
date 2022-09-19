2767544f34cb1855b97cc859697ef03ddb2d6b9f
AtifSaleem37 <108580110+AtifSaleem37@users.noreply.github.com>
2022-09-19 03:42:29 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
